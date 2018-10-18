454dade62d33320de98fb9463d2910f2163df9f3
Justin Gondron <jgondron@nd.edu>
2018-10-18 08:21:59 -0400

    MEL-89: Rename scripts directory used by codebuild to scripts/codebuild to avoid conflicts

--- scripts/build/local.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-./scripts/build/install.sh || { exit 1; }
-./scripts/build/pre_build.sh || { exit 1; }
-./scripts/build/build.sh || { exit 1; }
-./scripts/build/post_build.sh || { exit 1; }
