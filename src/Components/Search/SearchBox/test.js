84f877f103b0e2cab39f3ad1a61e2b118499bade
jmille79 <jmille79@nd.edu>
2019-01-16 12:47:31 -0500

    Temp adjustments

--- src/Components/Search/SearchBox/test.js
+++ src/Components/Search/SearchBox/test.js
@@ -23,9 +23,3 @@ test('Search component contains a form, an input, and a button', () => {
   expect(wrapper.find('input').exists()).toBeTruthy()
   expect(wrapper.find('button').exists()).toBeTruthy()
 })
-
-test('Search Box dispatches the searchAction#submitSearch function with the value of the input field', () => {
-  wrapper.find('input').simulate('change', { target: { value: 'test' } })
-  wrapper.find('form').simulate('submit', { e: wrapper.find('button').at(0), dispatch: dispatch })
-  expect(dispatch).toHaveBeenCalled()
-})
