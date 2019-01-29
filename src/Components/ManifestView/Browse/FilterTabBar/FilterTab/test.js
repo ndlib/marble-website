f9658540263744bff22f49ce5ea63c7f94d8f524
Dan Wolfe <dwolfe2@nd.edu>
2019-01-29 14:26:36 -0500

    forgot to save test.js

--- src/Components/ManifestView/Browse/FilterTabBar/FilterTab/test.js
+++ src/Components/ManifestView/Browse/FilterTabBar/FilterTab/test.js
@@ -5,8 +5,6 @@ import FilterTab from './'
 
 const wrapper = shallow(<FilterTab label='LABEL' target='/target' />)
 
-test('Renders a link to target with a label', () => {
-  console.log(wrapper.find('.filterTab').text())
+test('Renders a link to target', () => {
   expect(wrapper.find(Link).prop('to')).toEqual('/target')
-  expect(wrapper.find('.filterTab').text()).toEqual('LABEL')
 })
