0043272f0eac0c71eef9b701bc2b6b03bc3bb5cf
Dan Wolfe <dwolfe2@nd.edu>
2019-02-05 11:04:16 -0500

    sanitize terms

--- src/Functions/searchUrl/index.js
+++ src/Functions/searchUrl/index.js
@@ -1,3 +1,4 @@
 export default (terms, perpage, page, view) => {
+  // Note we are sanitizing user input with encodeURIComponent
   return `/search?terms=${encodeURIComponent(terms)}&perpage=${perpage}&page=${page}&view=${view}`
 }
