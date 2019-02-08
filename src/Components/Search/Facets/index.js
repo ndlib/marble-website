7380b2467c94d3f93cabbae46ce94e45cda1eab9
jmille79 <jmille79@nd.edu>
2019-02-08 12:49:29 -0500

    Suggested Updates

--- src/Components/Search/Facets/index.js
+++ src/Components/Search/Facets/index.js
@@ -5,38 +5,7 @@ import { connect } from 'react-redux'
 import { withRouter } from 'react-router'
 import searchUrl from 'Functions/searchUrl'
 import './style.css'
-
-const repo = [
-  { value: 'RBSC', label: 'Rare Books and Special Collections Department' },
-  { value: 'Snite', label: 'Snite Museum of Art' },
-  { value: 'Archives', label: 'University Archives' },
-]
-
-const format = [
-  { value: 'Book', label: 'Book' },
-  { value: 'DA', label: 'Decorative Arts' },
-  { value: 'Ephemera', label: 'Ephemera' },
-  { value: 'Ethno', label: 'Ethnographic' },
-  { value: 'Manuscript', label: 'Manuscript' },
-  { value: 'Painting', label: 'Painting' },
-  { value: 'Photo', label: 'Photograph' },
-  { value: 'Sculpture', label: 'Sculpture' },
-]
-
-const language = [
-  { value: 'English', label: 'English' },
-  { value: 'French', label: 'French' },
-  { value: 'Latin', label: 'Latin' },
-  { value: 'Italian', label: 'Italian' },
-  { value: 'Spanish', label: 'Spanish' },
-  { value: 'Chinese', label: 'Chinese' },
-  { value: 'Multiple', label: 'Multiple Languages' },
-]
-
-const options = [
-
-]
-
+import { repo, format, language, options } from 'Configurations/Search/facets'
 export const Facets = ({ searchReducer }) => {
   return (
     <div className='facets'>
---
+++ src/Configurations/Search/facets.js
@@ -0,0 +1,85 @@
+
+export const repo = [
+  {
+    value: 'RBSC',
+    label: 'Rare Books and Special Collections Department',
+  },
+  {
+    value: 'Snite',
+    label: 'Snite Museum of Art',
+  },
+  {
+    value: 'Archives',
+    label: 'University Archives',
+  },
+]
+
+export const format = [
+  {
+    value: 'Book',
+    label: 'Book',
+  },
+  {
+    value: 'DA',
+    label: 'Decorative Arts',
+  },
+  {
+    value: 'Ephemera',
+    label: 'Ephemera',
+  },
+  {
+    value: 'Ethno',
+    label: 'Ethnographic',
+  },
+  {
+    value: 'Manuscript',
+    label: 'Manuscript',
+  },
+  {
+    value: 'Painting',
+    label: 'Painting',
+  },
+  {
+    value: 'Photo',
+    label: 'Photograph',
+  },
+  {
+    value: 'Sculpture',
+    label: 'Sculpture',
+  },
+]
+
+export const language = [
+  {
+    value: 'English',
+    label: 'English',
+  },
+  {
+    value: 'French',
+    label: 'French',
+  },
+  {
+    value: 'Latin',
+    label: 'Latin',
+  },
+  {
+    value: 'Italian',
+    label: 'Italian',
+  },
+  {
+    value: 'Spanish',
+    label: 'Spanish',
+  },
+  {
+    value: 'Chinese',
+    label: 'Chinese',
+  },
+  {
+    value: 'Multiple',
+    label: 'Multiple Languages',
+  },
+]
+
+export const options = [
+
+]
