93da0311d2f0a5bc3e635a7e48b8a20b6ac3859f
Dan Wolfe <dwolfe2@nd.edu>
2019-02-11 15:45:52 -0500

    hound

--- src/Components/Shared/DisplayViewClass/index.js
+++ src/Components/Shared/DisplayViewClass/index.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { connect } from 'react-redux'
 
 const DisplayViewClass = ({ children, reducer }) => {
   const { view } = reducer
