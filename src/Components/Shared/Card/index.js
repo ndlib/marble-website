c8622fcaee397b021e32baf34ebe4abf26788cea
Dan Wolfe <dwolfe2@nd.edu>
2019-02-28 14:57:14 -0500

    remove unused component

--- src/Components/Shared/Card/index.js
+++ src/Components/Shared/Card/index.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { Link } from 'react-router-dom'
 import { withRouter } from 'react-router'
 import urlContext from 'Functions/urlContext'
 import './style.css'
