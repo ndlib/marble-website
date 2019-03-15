e327954e9dac77deed5e8fde17b5101302367de0
Andy Wetherill <awetheri@nd.edu>
2019-03-15 11:00:22 -0400

    Updates to home page layout and cleaning up styling on text across the site.

--- src/Components/Home/HomeBanner/index.js
+++ src/Components/Home/HomeBanner/index.js
@@ -11,11 +11,11 @@ const HomeBanner = () => {
       />
       <div className='imageCaption'>
         <div className='captionFrame'>
-          <h1>Explore digitized artwork, rare books, artifacts, and archival materials from the Snite Museum of Art and Hesburgh Libraries.</h1>
+          <h1>Explore digitized artwork, rare books, artifacts, and archival materials from the University of Notre Dame.</h1>
         </div>
       </div>
       <div className='imageCitation'>
-        <p className='citation'><sub>Unrecorded Chinese artists,  <em>Untitled: Patriarch En Route with Attendants</em>,  20th century, woodcut with watercolor. <br />Snite Museum of Art, University of Notre Dame, Transfer from the Rare Books and Special Collections Department, 1977.005.045.</sub></p>
+        <p className='citation'><sub>Photograph of St. Joseph's Novitiate exterior from across St. Joseph's Lake, 1894,<br /> Notre Dame Life Photograph Collection (GNDL) 45/0, University of Notre Dame Archives.</sub></p>
       </div>
     </div>
   )
