e73a6a071acc67fa681852540e967d7e9fdb4c15
Dan Wolfe <dwolfe2@nd.edu>
2019-03-20 10:09:26 -0400

    Add comment

--- src/Components/ManifestView/Item/ItemSide/ItemAlternateViews/AlternateImage/AlternateOverlay/index.js
+++ src/Components/ManifestView/Item/ItemSide/ItemAlternateViews/AlternateImage/AlternateOverlay/index.js
@@ -2,6 +2,8 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 const AlternateOverlay = ({ index, max, length }) => {
+  // only render on last instance
+  // do not render if total shown equals total available
   if (max === index + 1 && max !== length) {
     const overlayNumber = length - max
     return (
