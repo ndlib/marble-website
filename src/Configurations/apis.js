071871da3895753c6548d9300bcc77e25ba38cfa
Dan Wolfe <dwolfe2@nd.edu>
2019-03-22 13:59:05 -0400

    comment

--- src/Configurations/apis.js
+++ src/Configurations/apis.js
@@ -9,6 +9,8 @@ export const SENTRY_DSN = 'https://4c1993c43f954b99b98e53fdf0a594f2@sentry.io/13
 // Primo base url can be destroyed and rebuilt so it will get a new url
 export const PRIMO_BASE_URL = 'https://a1fc3ld3d7.execute-api.us-east-1.amazonaws.com/dev/primo/v1/search'
 
-export const MANIFEST_BASE_URL = 'https://d1v1nx8kcr1acm.cloudfront.net/'
+// TODO: UPDATE manifests on production uri
 // export const MANIFEST_BASE_URL = 'https://presentation-iiif.library.nd.edu'
+export const MANIFEST_BASE_URL = 'https://d1v1nx8kcr1acm.cloudfront.net/'
+
 export const UNIVERSAL_VIEWER_BASE_URL = 'https://viewer-iiif.library.nd.edu/universalviewer/index.html'
