9cd6fe75b1a6e60b9a3189748bf7df16b54e354e
Dan Wolfe <dwolfe2@nd.edu>
2019-03-28 13:30:54 -0400

    Add a little css helping class at a high level

--- src/Components/App/Page/index.js
+++ src/Components/App/Page/index.js
@@ -5,9 +5,17 @@ import './style.css'
 import Header from './Header'
 import NavBar from './NavBar'
 import Footer from './Footer'
+import {
+  BROWSE_CONTEXT,
+  COLLECTION_CONTEXT,
+  ITEM_CONTEXT,
+  VIEWER_CONTEXT,
+  EXHIBITIONS_CONTEXT,
+  SEARCH_CONTEXT,
+} from 'Constants/viewingContexts'
 
 export const Page = ({ children, location }) => {
-  const contentClass = location.pathname === '/' ? 'home mainContent' : 'mainContent'
+  const contentClass = getClass(location.pathname)
 
   return (
     <React.Fragment>
@@ -30,3 +38,25 @@ Page.propTypes = {
   location: PropTypes.object.isRequired,
 }
 export default withRouter(Page)
+// eslint-disable-next-line complexity
+export const getClass = (pathname) => {
+  let contentClass
+  if (pathname === '/') {
+    contentClass = 'home mainContent'
+  } else if (pathname.indexOf(BROWSE_CONTEXT) === 1) {
+    contentClass = 'browse mainContent'
+  } else if (pathname.indexOf(COLLECTION_CONTEXT) === 1) {
+    contentClass = 'collection mainContent'
+  } else if (pathname.indexOf(ITEM_CONTEXT) === 1) {
+    contentClass = 'item mainContent'
+  } else if (pathname.indexOf(VIEWER_CONTEXT) === 1) {
+    contentClass = 'viewer mainContent'
+  } else if (pathname.indexOf(EXHIBITIONS_CONTEXT) === 1) {
+    contentClass = 'exhibitions mainContent'
+  } else if (pathname.indexOf(SEARCH_CONTEXT) === 1) {
+    contentClass = 'search mainContent'
+  } else {
+    contentClass = 'mainContent'
+  }
+  return contentClass
+}
