c4e7ec21ea8e7b3f5bd23b71fd99fa56d7604b41
jmille79 <jmille79@nd.edu>
2019-05-03 09:30:15 -0400

    Added testing, allowed input props

--- src/Components/MetaTest/index.js
+++ src/Components/MetaTest/index.js
@@ -1,26 +1,32 @@
 import React from 'react'
-import schema from 'Configurations/Schema/basicschema2.json'
+import basicschema from 'Configurations/Schema/basicschema2.json'
 import renderer from 'Configurations/Schema/rendering.json'
 
-const MetaTest = () => {
-  var metaProp = []
+const MetaTest = (props) => {
+  let schema = props.schema != null ? props.schema : basicschema
   var metaObj = []
   renderer.sections[0].attributes.forEach(function (field) {
     if (schema[field.key] != null) {
       switch (field.renderer) {
         case 'title':
-          metaObj.push(<div className={schema[field.key]}> {schema[field.key]} </div>)
+          metaObj.push(<h1 itemProp='name' className={field.key}> {schema[field.key]} </h1>)
           break
         case 'basic':
         default:
-          metaObj.push(<React.Fragment><dt>{field.label}:</dt><dd> {schema[field.key]} </dd></React.Fragment>)
+          metaObj.push(<React.Fragment><dt>{field.label}:</dt><dd itemProp={field.key} className={field.key}> {schema[field.key]} </dd></React.Fragment>)
       }
     }
   })
   return (
-    <dl>
-      {metaObj}
-    </dl>
+    <div
+      itemScope
+      itemType='http://schema.org/CreativeWork'
+      className='schemaOrgMetadata'
+    >
+      <dl>
+        {metaObj}
+      </dl>
+    </div>
   )
 }
 
